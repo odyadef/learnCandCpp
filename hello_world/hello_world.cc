70567030469785bf16c3e00f40b385ed480d4e28
Odya Def <odyadef@gmail.com>
2025-04-03 16:59:07 +0400

    update hello_world

--- hello_world/hello_world.cc
+++ hello_world/hello_world.cc
@@ -1,7 +1,13 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "ППППП  РРРРР  И   И  ВВВВВ  ЕЕЕЕЕ  ТТТТТ  !! " << endl
+         << "П   П  Р   Р  И  ИИ  В   В  Е        Т    !! " << endl
+         << "П   П  РРРРР  И И И  ВВВВ   ЕЕЕЕЕ    Т    !! " << endl
+         << "П   П  Р      ИИ  И  В   В  Е        Т       " << endl
+         << "П   П  Р      И   И  ВВВВВ  ЕЕЕЕЕ    Т    !! " << endl;
     return 0;
 }
